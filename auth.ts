4ba67348077615583897a0fc966f95317de2fb60
Vikram Naik <26vikramnaik@gmail.com>
2024-06-21 20:30:53 +0530

    fix(session): add role field to session, extend session & token types

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter"
 
 import authConfig from "@/auth.config"
 import { db } from "./lib/db"
+import { getUserById } from "./data/users"
 
 export const {
   handlers,
@@ -10,6 +11,29 @@ export const {
   signOut,
   auth
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role
+      }
+
+      return session
+    },
+
+    async jwt({ token }) {
+      if (!token.sub) return token
+
+      const existingUser = await getUserById(token.sub)
+      if (!existingUser) return token
+
+      token.role = existingUser.role
+      return token
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig
